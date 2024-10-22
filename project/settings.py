41e6484e4b1b6396d99e6e276b14701613630fc1
Roshdy <roshdysaid72@gmail.com>
2024-10-22 17:34:06 +0300

    4

--- project/settings.py
+++ project/settings.py
@@ -80,13 +80,13 @@ LOGIN_URL = '/login/'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
