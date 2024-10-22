563fea0387fe6aef5fa440673e7c5aceb1bfe623
Roshdy <roshdysaid72@gmail.com>
2024-10-22 17:23:25 +0300

    3

--- Procfile
+++
@@ -1,2 +0,0 @@
-web: gunicorn Electronic-real-estate-marketing-company.wsgi --log-file -
-worker: celery -A Electronic-real-estate-marketing-company worker --loglevel=info
--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&%&+42huto96xl$1tc3t&smb)j57wpb*uu(tdytq0u0(#8t2b-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
